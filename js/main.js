16b0092d83e4340f1dbbeec203842099b6d651e2
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-21 16:57:29 +0200

    feat: bonus 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-for (let i = 0; i <= 100; i++) {
+const fizzbuzz_row = document.querySelector('div.fizzbuzz_row');
+
+
+for (let i = 1; i <= 100; i++) {
+    let divBox = `<div class="box color_number">${i}</div>`
     if (i % 3 == 0 || i % 5 == 0 ) {
         if (i % 3 == 0 && i % 5 == 0 ) {
+            divBox = `<div class="box color_fizzbuzz">FizzBuzz</div>`
+            fizzbuzz_row.innerHTML += divBox;
             console.log('FizzBuzz');
         }else if (i % 3 == 0) {
+            divBox = `<div class="box color_fizz">Fizz</div>`
+            fizzbuzz_row.innerHTML += divBox;
             console.log('Fizz');
         }else {
+            divBox = `<div class="box color_buzz">Buzz</div>`
+            fizzbuzz_row.innerHTML += divBox;
             console.log('Buzz');
         }
     }else {
+        fizzbuzz_row.innerHTML += divBox;
         console.log(i);
     }
 }
