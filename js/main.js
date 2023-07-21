3df1b54e3536081f97fe4c43eac8771f8a655ba2
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-21 17:38:19 +0200

    fix: method .append() + .getElementById()

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
-const fizzbuzz_row = document.querySelector('div.fizzbuzz_row');
-
+const fizzbuzz_row = document.getElementById('fizzbuzz_row');
+// trasforma in id
 
 for (let i = 1; i <= 100; i++) {
-    let divBox = `<div class="box color_number">${i}</div>`
+    // let divBox = `<div class="box color_number">${i}</div>`
+    const divBox = document.createElement("div")
+    divBox.classList.add('box')
+
+    
     if (i % 3 == 0 || i % 5 == 0 ) {
         if (i % 3 == 0 && i % 5 == 0 ) {
-            divBox = `<div class="box color_fizzbuzz">FizzBuzz</div>`
-            fizzbuzz_row.innerHTML += divBox;
-            console.log('FizzBuzz');
+            // divBox = `<div class="box color_fizzbuzz">FizzBuzz</div>`
+            // fizzbuzz_row.innerHTML += divBox;
+            divBox.classList.add('color_fizzbuzz')
+            divBox.innerHTML = 'FizzBuzz';
+            fizzbuzz_row.append(divBox);
+            console.log(divBox);
+            // console.log('FizzBuzz');
         }else if (i % 3 == 0) {
-            divBox = `<div class="box color_fizz">Fizz</div>`
-            fizzbuzz_row.innerHTML += divBox;
-            console.log('Fizz');
+            // divBox = `<div class="box color_fizz">Fizz</div>`
+            // fizzbuzz_row.innerHTML += divBox;
+            divBox.classList.add('color_fizz')
+            divBox.innerHTML = 'Fizz';
+            fizzbuzz_row.append(divBox);
+            // console.log('Fizz');
         }else {
-            divBox = `<div class="box color_buzz">Buzz</div>`
-            fizzbuzz_row.innerHTML += divBox;
-            console.log('Buzz');
+            // divBox = `<div class="box color_buzz">Buzz</div>`
+            // fizzbuzz_row.innerHTML += divBox;
+            divBox.classList.add('color_buzz')
+            divBox.innerHTML = 'Buzz';
+            fizzbuzz_row.append(divBox);
+            // console.log('Buzz');
         }
     }else {
-        fizzbuzz_row.innerHTML += divBox;
-        console.log(i);
+        // fizzbuzz_row.innerHTML += divBox;
+        divBox.classList.add('color_number')
+        divBox.innerHTML = i;
+        fizzbuzz_row.append(divBox);
+        // console.log(i);
     }
 }
